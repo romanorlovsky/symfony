4b6166470867a3e27cf5b8e690b9ec3ba261d973
roman.orlovskyi <roman.orlovskyi@masterofcode.com>
2013-11-14 14:11:33 +0200

    update main controller

--- src/Acme/DemoBundle/Controller/ArticleController.php
+++ src/Acme/DemoBundle/Controller/ArticleController.php
@@ -11,41 +11,57 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 
+use Acme\DemoBundle\Entity\Article;
+use Acme\DemoBundle\Entity\Page;
+
 class ArticleController extends Controller
 {
 
     public function indexAction()
     {
-        $article = array(
-            'title' => 'Home',
-            'project_name' => 'Article'
-        );
+        $home = new Page();
+        $home->setTitle('Home');
+        $home->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
-        return $this->render('AcmeDemoBundle:Article:index.html.twig', $article);
+        $articles = array();
+
+        for ($i = 0; $i < 5; ++$i) {
+            $article = new Article();
+            $article->setTitle('Article ' . ($i + 1));
+            $article->setCreated(time());
+            $article->setContent('Description ' . ($i + 1));
+
+            $articles[] = $article;
+        }
+
+        return $this->render('AcmeDemoBundle:Article:index.html.twig', array(
+                'page' => $home,
+                'articles' => $articles
+            )
+        );
     }
 
     public function aboutAction()
     {
-        $article = array(
-            'id' => 1,
-            'title' => 'About',
-            'desc' => 'Hello world',
-            'project_name' => 'Article'
-        );
+        $about = new Page();
+        $about->setTitle('About');
+        $about->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
-        return $this->render('AcmeDemoBundle:Article:about.html.twig', $article);
+        return $this->render('AcmeDemoBundle:Article:about.html.twig', array(
+                'page' => $about
+            )
+        );
     }
 
     public function contactAction()
     {
-        $article = array(
-            'id' => 1,
-            'title' => 'Contact',
-            'desc' => 'Hello world',
-            'page_title' => 'Contact',
-            'project_name' => 'Article'
-        );
+        $contact = new Page();
+        $contact->setTitle('Contact');
+        $contact->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
 
-        return $this->render('AcmeDemoBundle:Article:contact.html.twig', $article);
+        return $this->render('AcmeDemoBundle:Article:contact.html.twig', array(
+                'page' => $contact
+            )
+        );
     }
 }
---
+++ src/Acme/DemoBundle/Entity/Article.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Acme\DemoBundle\Entity;
+
+
+class Article
+{
+    protected $title;
+    protected $content;
+    protected $created;
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $created
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+}
